32f9f1d889c07ac6a2fa00d7f188f68a7f34300e
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:25:23 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "NUEVA CEPA DEL COVID-19",
+"autor" => "Anthony Manuel Chavez Lujan",
+"resumen" => "Se encontro una nueva cepa del covid 19",
 ];
 ?>
