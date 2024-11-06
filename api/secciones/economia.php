6698efcc03adc794d85db895682a9a96adbec2b2
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:28:10 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Subio el dolar",
+"autor" => "Cristofer",
+"resumen" => "SUBIO EL PRECIO DEL DOLAR",
 ];
 ?>
