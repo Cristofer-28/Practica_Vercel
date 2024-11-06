7f81c1f846571c4b42ff800afd33a67f849cba9b
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:31:14 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El entrenamiento militar de la princesa Victoria de Suecia, en imágenes",
+"autor" => "Victoria de Suecia",
+"resumen" => "Disparando fusiles automáticos, utilizando artillería de mortero, atravesando un río, desplazándose por cuerdas suspendidas, de rodillas y completando circuitos complicados. La casa real noruega publica las imágenes de la preparación militar básica de su heredera con el Regimiento Anfibio de Estocolmo",
 ];
 ?>
