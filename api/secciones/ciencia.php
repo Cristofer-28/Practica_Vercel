ae3b064cfcb3b6dfce0b74bf9ffc796745107c2b
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:42:38 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,14 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "NUEVA CEPA DEL COVID-19",
-"autor" => "Anthony Manuel Chavez Lujan",
-"resumen" => "Se encontro una nueva cepa del covid 19",
+"titulo" => "Mitos y verdades sobre la infertilidad femenina",
+"autor" => "TV Perú",
+"resumen" => "La infertilidad es un problema que el 40 % de veces ocurre en las mujeres, 40 % en los varones y un 20 % en ambos.
+
+Sin embargo, social y culturalmente, casi siempre se asume que la infertilidad es un tema que recae en las mujeres, creando estigmas y mitos que no permiten abordarlo y llevar tratamientos, en caso se quiera tener un bebé.
+
+Según la Dra. Eva Obregón, ginecóloga, un factor importante es la edad. Cuando una pareja intenta tener un hijo en un año, pero no lo logra, y la mujer tiene menos de 35 años, entonces hay una alta probabilidad que sea infértil. Este patrón se repite si la mujer tiene entre 35 a 39 años y en 6 meses tampoco se embaraza, o si tiene entre 40 años a más y en 2 a 3 meses no lo logra.
+
+“Si una mujer, a los 20 años, quiere embarazarse, tiene el 5 % de chance de no lograrlo; a los 30, tiene un 20 %; y después de los 40, la mitad no lo va a lograr”, explicó la especialista.",
 ];
 ?>
