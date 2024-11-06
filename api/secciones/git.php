6d4a1c29e45cb04b004475fef9d62b94b2db6ec2
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:32:27 -0500

    Update git.php

--- api/secciones/git.php
+++ api/secciones/git.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $git = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "GitHub despedirá al 10% de su plantilla y cerrará sus oficinas para adoptar el teletrabajo a tiempo completo",
+"autor" => "RPP Noticias",
+"resumen" => "GitHub, empresa adquirida por Microsoft, se suma al resto de subsidiarias de la empresa tecnológica que hará recortes de personal este año.",
 ];
 ?>
