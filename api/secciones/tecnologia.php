d4b471cd0e0c1041601c84cdafc130dadc63e3d9
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:37:58 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Apps para negocios: Estrategias para mantenerlas al día y maximizar su impacto",
+"autor" => "Carlos Rosales Salas",
+"resumen" => "El uso de aplicaciones, o simplemente apps, ha permitido que las empresas puedan agilizar procesos, mejorar la visibilidad de sus productos, personalizar la experiencia de sus clientes y sobre todo aumentar las ventas.",
 ];
 ?>
