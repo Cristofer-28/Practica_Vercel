0b05cea4cc1814c881787f4972b33d52499e8999
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:35:30 -0500

    Update openstack.php

--- api/secciones/openstack.php
+++ api/secciones/openstack.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openstack = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Red Hat OpenStack Platform 13 simplifica las actualizaciones con funciones de avance rápido",
+"autor" => "Redacción Byte TI",
+"resumen" => "Red Hat ha anunciado Red Hat OpenStack Platform 13, la última versión de la solución de infraestructura como servicio (IaaS) en la nube masivamente escalable y ágil de Red Hat. Basado en la versión OpenStack «Queens», Red Hat OpenStack Platform 13 proporciona una base potente para las empresas que utilizan la nube híbrida en sus estrategias de transformación digital, ayuda a minimizar los problemas de crecimiento que a menudo conlleva la adopción de nuevas tecnologías con su nueva función de actualización Fast Forward, y está diseñado para mejorar la integración en toda la gama de tecnologías emergentes.",
 ];
 ?>
