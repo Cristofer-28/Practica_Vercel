231cc618ee5d1df1660d3161a87471125ee21064
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:27:42 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Así será el Festival de Danza Contemporánea",
+"autor" => "MIjail",
+"resumen" => "Esta noche es especial para la comunidad dancística. La Escuela Nacional Superior de Ballet (ENSB) presenta el II Festival de Danza Contemporánea.",
 ];
 ?>
