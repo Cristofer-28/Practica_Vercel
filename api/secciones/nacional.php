0e2e78cca241f4596a44ae79685cc9e178c2c23e
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:33:18 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Perú Libre rechaza acuerdo para que policías y militares sean juzgados en otro fuero",
+"autor" => "GESTIÓN",
+"resumen" => "Flavio Cruz cuestiona el papel de las autoridades del Ministerio Público y el Poder Judicial por no oponerse a la medida en reunión del Consejo de Estado",
 ];
 ?>
