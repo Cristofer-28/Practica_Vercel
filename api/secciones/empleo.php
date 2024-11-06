2f5d690aefa80d08738bbe21d080dcb6acfb51dd
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:28:52 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "SE ABRE CONVOCATORIA LABORAL",
+"autor" => "MARIAFERNANDA",
+"resumen" => "se abrio nueva convocatoria laboral",
 ];
 ?>
