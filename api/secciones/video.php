35ccd0832ec028a0210fe65ec43f9120a4e8bf5b
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:40:22 -0500

    Update video.php

--- api/secciones/video.php
+++ api/secciones/video.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $video = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "¿Piero Quispe será papá? Así fue su emotiva celebración de gol con Pumas",
+"autor" => "Brenda García Retamal",
+"resumen" => "Piero Quispe sigue demostrando su talento en el extranjero tras anotar un gol en el minuto 83 que selló el triunfo de Pumas UNAM sobre Querétaro como parte de la Liga MX. Después de ello, el centrocampista sorprendió a todos sus seguidores con su celebración dejando entrever que se convertirá en padre.",
 ];
 ?>
