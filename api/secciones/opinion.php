07e19f9cf24df07508ae0d152d2f2a00f59da423
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:36:11 -0500

    Update opinion.php

--- api/secciones/opinion.php
+++ api/secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "APEC: modelo de crecimiento económico y desarrollo",
+"autor" => "Elmer Schialer Salcedo",
+"resumen" => "APEC Perú 2024 va llegando a su punto culminante con la próxima realización de la Cumbre de Líderes. Las 21 economías de APEC, que representan el 62% del PBI mundial, el 48% del comercio y el 38% de la población globales, vendrán convocadas por el Perú para rubricar una agenda de trabajo sustantiva, propuesta por nuestro país y que será adoptada unánimemente la próxima semana. ",
 ];
 ?>
