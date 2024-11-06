4e56bfb5e6917791531225c48de1485fa7c4dce8
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:39:23 -0500

    Update television.php

--- api/secciones/television.php
+++ api/secciones/television.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Netflix anuncia la eliminación de prácticamente todas sus películas y series interactivas en diciembre",
+"autor" => "Carlos Rodríguez",
+"resumen" => "En 2017, Netflix introdujo una innovadora modalidad en su plataforma: los títulos interactivos. El primer lanzamiento de este tipo fue El Gato con Botas: Atrapado en un cuento épico. Sin embargo, no fue hasta 2018, con la llegada de Black Mirror: Bandersnatch, que este formato realmente se convirtió en tendencia y capturó la atención mundial. Ahora, este llegaría a su final. ",
 ];
 ?>
