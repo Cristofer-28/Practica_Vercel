552cf496da6bcc79dc5a9efd31e5581e2b6c9f39
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:44:04 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,10 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+"titulo" => "Perú y China firman memorándum para fomentar la cooperación en el ámbito educativo",
+"autor" => "TV Perú",
+"resumen" => "Perú y China suscribieron un memorándum de entendimiento para fomentar el intercambio y la cooperación en todos los niveles del ámbito de la educación. El acto se realizó con la participación del ministro de Educación, Morgan Quero, y el viceministro de Educación de la República Popular China, Jie Chen.
+
+El funcionario chino expresó su alegría de estar en el Perú y afirmó que su país da mucha importancia a esta visita y a las modalidades de cooperación que pueden acordarse. Recordó que se reunió en Brasil con el ministro Morgan Quero y ambos coincidieron en la necesidad de fortalecer las relaciones bilaterales.",
 ];
 ?>
