f85d4c03cb75c1a4354c5396bc069357bc7f0102
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:41:28 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,10 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "Subio el dolar",
-"autor" => "Cristofer",
-"resumen" => "SUBIO EL PRECIO DEL DOLAR",
+"titulo" => "Precio del dólar hoy en Perú: ¿cuánto está el tipo de cambio este miércoles 6 de noviembre?",
+"autor" => "TV Perú Noticias",
+"resumen" => "Dólar en Perú. Al inicio de la jornada de hoy, miércoles 6 de noviembre, el precio del dólar estadounidense en Perú se sitúa en S/3.774.
+
+El tipo de cambio corresponde al cierre del martes 5 de noviembre, reportado por el Banco Central de Reserva del Perú. Es decir, el precio del dólar se mantuvo en S/3.774.",
 ];
 ?>
