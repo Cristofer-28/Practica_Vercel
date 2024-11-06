e1ffab5a91a4abbe29531ed3f672a78a3a4d46f6
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:34:23 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,9 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Red Hat lleva la plataforma de automatización Ansible líder del sector a Microsoft Azure",
+"autor" => "rED HAT",
+"resumen" => "AMÉRICA LATINA - 8 de diciembre de 2021 —
+Red Hat, Inc., el proveedor líder mundial de soluciones open source, anunció hoy la incorporación de Red Hat Ansible Automation Platform a Microsoft Azure, llevando la tecnología de automatización líder a una de las principales nubes públicas del mundo. Esta colaboración entre Red Hat y Microsoft Azure brinda a las organizaciones acceso a una solución poderosa que les ofrece flexibilidad al adoptar la automatización para poder proveer cualquier aplicación, en cualquier lugar, sin mayor complejidad ni costos fijos adicionales.",
 ];
 ?>
