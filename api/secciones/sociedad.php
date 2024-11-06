9481c19bd39343c2f799b293e92533611b73daee
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:36:53 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Huelga de controladores aéreos provoca retrasos en el Aeropuerto Jorge Chávez: pasajeros varados tras cancelación de vuelos",
+"autor" => "Valeria Tosso",
+"resumen" => "Pasajeros reportan retrasos en los vuelos nacionales e internacionales en el Aeropuerto Internacional Jorge Chávez debido a la huelga anunciada por el Sindicato Nacional de Especialistas Aeronáuticos de CORPAC S.A. (Sineacor), que se llevará a cabo del 13 al 15 de noviembre, coincidiendo con la Cumbre APEC. Los retrasos han llegado hasta una hora y algunos vuelos incluso han sido cancelados, lo que ha alterado la programación habitual del aeropuerto.",
 ];
 ?>
