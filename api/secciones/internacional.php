05f6c4cd991150899e15966f8497f0f0ca829f23
Cristofer-28 <155126351+Cristofer-28@users.noreply.github.com>
2024-11-06 11:29:45 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "DONALD TRUMP NUEVO PRESIDNETE",
+"autor" => "Noticias America",
+"resumen" => "se eligio a Donald Trump como nuevo presidente de EE.UU",
 ];
 ?>
